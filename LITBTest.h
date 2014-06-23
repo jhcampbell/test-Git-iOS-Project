9560bf6adb37e5f36f898cdfb19384c5f017a5f7
Jonathan <jonathan@stc-imac-jc.pacer.org>
2014-06-23 16:10:42 -0500

    added test class

---
+++ LITBTest.h
@@ -0,0 +1,13 @@
+//
+//  LITBTest.h
+//  Git Test
+//
+//  Created by Jonathan on 6/23/14.
+//  Copyright (c) 2014 Jonathan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LITBTest : NSObject
+
+@end
---
+++ LITBTest.m
@@ -0,0 +1,13 @@
+//
+//  LITBTest.m
+//  Git Test
+//
+//  Created by Jonathan on 6/23/14.
+//  Copyright (c) 2014 Jonathan. All rights reserved.
+//
+
+#import "LITBTest.h"
+
+@implementation LITBTest
+
+@end
